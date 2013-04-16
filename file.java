396a7e77c850001fca92dcfe8532571a0a114222
MakiCode <trentonmaki@gmail.com>
2013-04-16 10:36:23 -0600

    ititail commit from remote repo

--- file.java
+++ file.java
@@ -1,5 +1,5 @@
 public class file {
   public static void main(String[] args) {
-    System.out.println("Testing");
+    System.out.println("Testing YAY I have just pulled and pushed from a remote repo!");
   }
 }
